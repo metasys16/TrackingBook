6539be2aa55ba89660f9820e78782e0e18ac54ec
metasys16 <pereira.mickael1@gmail.com>
2016-11-18 09:57:12 +0100

    Ajout Livre

--- TrackingBook/Modele/Editeur.cs
+++ TrackingBook/Modele/Editeur.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TrackingBook
 {
-    class Editeur
+    class Editeur : INotifyPropertyChanged
     {
 
         private String nom;
@@ -14,7 +15,23 @@ namespace TrackingBook
         public String Nom
         {
             get { return nom; }
-            set { nom = value; }
+            set
+            {
+                if (nom != value)
+                {
+                    nom = value;
+                    this.NotifyPropertyChanged("Editeur");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string editeur)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(editeur));
+            }
         }
 
     }
--- TrackingBook/ajoutLivre.xaml.cs
+++ TrackingBook/ajoutLivre.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Modele;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,30 @@ namespace TrackingBook
     /// </summary>
     public partial class AjoutLivre : Page
     {
+
         public AjoutLivre()
         {
             InitializeComponent();
+        }
+        private void btnAjoutLivre_Click(object sender, RoutedEventArgs e)
+        {
+            Oeuvre oeuvre = new Oeuvre();
+            Auteur auteur = new Auteur();
+            Editeur editeur = new Editeur();
+
+            oeuvre.Titre = titre.Text;
+            oeuvre.Resume = resume.Text;
+
+            // VERIFICATION ISBSN unique
+            oeuvre.ISBN10 = ISBN.Text;
+            auteur.Nom = nomAuteur.Text;
+            auteur.Prenom = prenomAuteur.Text;
+            editeur.Nom = nomEditeur.Text;
+        }
+
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
         }
     }
 }
